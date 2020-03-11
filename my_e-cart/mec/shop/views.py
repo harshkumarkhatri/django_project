996ee1bf55684c9bc625b261ef506cf7b1d6bb93
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-03-11 23:29:16 +0530

    Changed the callback url

--- my_e-cart/mec/shop/views.py
+++ my_e-cart/mec/shop/views.py
@@ -120,7 +120,7 @@ def checkout(request):
                 'INDUSTRY_TYPE_ID': 'Retail',
                 'WEBSITE': 'WEBSTAGING',
                 'CHANNEL_ID': 'WEB',
-                'CALLBACK_URL':'http://127.0.0.1:8000/shop/handlerequest/',
+                'CALLBACK_URL':'https://mera.harshblog.xyz/shop/handlerequest/',
 
         }
         param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
